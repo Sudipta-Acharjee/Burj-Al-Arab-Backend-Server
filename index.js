473c33368de26258c9e1152a554f220bc4ebdfab
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-03-29 15:19:40 +0600

    updated

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ client.connect(err => {
         }
     })
 });
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
